5b03e93a7d7ac452809396342db2acf10b89ad14
chen zefan <czfshine@outlook.com>
2019-03-22 14:24:35 +0800

    前端外部库

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'uxcore';
 import 'uxcore/assets/iconfont.css';
 import 'uxcore/assets/orange.css';
 import Demo from './page/product.js'
-import Index from './page/mater.js'
-
+import MiniDrawer from './components/MiniDrawer.js'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Demo/>
-          <Button> test Button</Button>
-          <Index/>
+          <MiniDrawer>
+            <Demo/>
+          </MiniDrawer>
       </div>
+
     );
   }
 }
--- web/src/withRoot.js
+++ web/src/withRoot.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import purple from 'material-ui/core/colors/purple';
-import green from 'material-ui/core/colors/green';
-import CssBaseline from 'material-ui/core/CssBaseline';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 // A theme with custom primary and secondary color.
 // It's optional.
