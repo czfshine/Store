9543cf558eb2b87a434a8daa33821944f5969cc0
chen zefan <czfshine@outlook.com>
2019-03-22 18:32:29 +0800

    前端增加rest支持

--- web/src/App.js
+++ web/src/App.js
@@ -4,12 +4,65 @@ import 'uxcore/assets/iconfont.css';
 import 'uxcore/assets/orange.css';
 import Demo from './page/product.js'
 import MiniDrawer from './components/MiniDrawer.js'
+
+import client from './client.js'
+class Employee extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.employee.name}</td>
+				<td>{this.props.employee.size}</td>
+				<td>{this.props.employee.description}</td>
+			</tr>
+		)
+	}
+}
+class EmployeeList extends React.Component{
+	render() {
+		const employees = this.props.employees.map(employee =>
+			<Employee key={employee._links.self.href} employee={employee}/>
+		);
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Description</th>
+					</tr>
+					{employees}
+				</tbody>
+			</table>
+		)
+	}
+}
+class B extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {employees: []};
+	}
+
+	componentDidMount() {
+    //TODO
+		client({method: 'GET', path: 'http://localhost:8080/data/products'}).done(response => {
+			this.setState({employees: response.entity._embedded.products});
+		});
+	}
+
+	render() {
+		return (
+			<EmployeeList employees={this.state.employees}/>
+		)
+	}
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
           <MiniDrawer>
-            <Demo/>
+            <B></B>
+            
           </MiniDrawer>
       </div>
 
---
+++ web/src/client.js
@@ -0,0 +1,33 @@
+const rest = require('rest');
+const defaultRequest = require('rest/interceptor/defaultRequest');
+const mime = require('rest/interceptor/mime');
+const errorCode = require('rest/interceptor/errorCode');
+const baseRegistry = require('rest/mime/registry');
+
+const registry = baseRegistry.child();
+
+var list=function() {
+
+	/* Convert a single or array of resources into "URI1\nURI2\nURI3..." */
+	return {
+		read: function(str /*, opts */) {
+			return str.split('\n');
+		},
+		write: function(obj /*, opts */) {
+			// If this is an Array, extract the self URI and then join using a newline
+			if (obj instanceof Array) {
+				return obj.map(resource => resource._links.self.href).join('\n');
+			} else { // otherwise, just return the self URI
+				return obj._links.self.href;
+			}
+		}
+	};
+
+}
+registry.register('text/uri-list', list);
+registry.register('application/hal+json', require('rest/mime/type/application/hal'));
+
+module.exports = rest
+		.wrap(mime, { registry: registry })
+		.wrap(errorCode)
+		.wrap(defaultRequest, { headers: { 'Accept': 'application/hal+json' }});
