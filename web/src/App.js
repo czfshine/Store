a474344c0f7ade5021c024cb98b0e94df165df3e
chen zefan <czfshine@outlook.com>
2019-03-23 13:34:25 +0800

    增加table库

--- web/src/App.js
+++ web/src/App.js
@@ -4,10 +4,10 @@ import 'uxcore/assets/iconfont.css';
 import 'uxcore/assets/orange.css';
 import Demo from './page/product.js'
 import MiniDrawer from './components/MiniDrawer.js'
-
 import client from './data/client.js'
-
 import TableShower from './components/TableShower.js'
+import MaterialTable from 'material-table'
+import './icon.css';
 class Employee extends React.Component{
 	render() {
 		return (
@@ -58,13 +58,46 @@ class B extends Component {
 		)
 	}
 }
+
 class App extends Component {
   render() {
     return (
       <div className="App">
           <MiniDrawer>
-            <TableShower/>
-        
+					<MaterialTable
+  columns={[
+    {
+      title: 'Avatar',
+      field: 'avatar',
+      render: rowData => (
+        <img
+          style={{ height: 36, borderRadius: '50%' }}
+          src={rowData.avatar}
+        />
+      ),
+    },
+    { title: 'Id', field: 'id' },
+    { title: 'First Name', field: 'first_name' },
+    { title: 'Last Name', field: 'last_name' },
+  ]}
+  data={query =>
+    new Promise((resolve, reject) => {
+      let url = 'https://reqres.in/api/users?'
+      url += 'per_page=' + query.pageSize
+      url += '&page=' + (query.page + 1)
+      fetch(url)
+        .then(response => response.json())
+        .then(result => {
+          resolve({
+            data: result.data,
+            page: result.page - 1,
+            totalCount: result.total,
+          })
+        })
+    })
+  }
+  title="Remote Data Example"
+/>
           </MiniDrawer>
       </div>
 
