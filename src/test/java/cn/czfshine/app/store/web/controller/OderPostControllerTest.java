2f9d17764aa630aaffe6d49055690ddd26900f90
czfshine <czfshine@outlook.com>
2019-06-17 21:38:45 +0800

    还原eslint配置

---
+++ src/test/java/cn/czfshine/app/store/web/controller/OderPostControllerTest.java
@@ -0,0 +1,12 @@
+package cn.czfshine.app.store.web.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OderPostControllerTest {
+
+    @Test
+    public void recvPost() {
+    }
+}
